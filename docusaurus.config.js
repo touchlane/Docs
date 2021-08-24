352de74be34da941a781fc89f4fd6169ec79e52d
Vadim Morozov <vmorozov@touchlane.com>
2021-08-24 21:16:39 +0300

    move navbar items to menu.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,21 +20,7 @@ module.exports = {
         alt: 'Touchlane Logo',
         src: 'img/logo.svg',
       },
-      items: [
-        {
-          type: 'doc',
-          docId: 'main',
-          position: 'left',
-          label: 'Docs',
-          activeSidebarClassName: 'navbar__link--active',
-          docsPluginId: 'default',
-        },
-        {
-          href: 'https://api.touchlane.com/api/oauth/github/login',
-          label: 'Sign In',
-          position: 'right',
-        },
-      ],
+      items: require('./menu.js'),
     },
     footer: {
       style: 'light',
---
+++ menu.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    type: 'doc',
+    docId: 'main',
+    position: 'left',
+    label: 'Docs',
+    activeSidebarClassName: 'navbar__link--active',
+    docsPluginId: 'default',
+  },
+  {
+    href: 'https://api.touchlane.com/api/oauth/github/login',
+    label: 'Sign In',
+    position: 'right',
+  }
+]
