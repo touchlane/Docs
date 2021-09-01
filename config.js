512fa6fd62a064ab278a94551aa843bce644bef9
Evgeny Dedovets <evgeny.dedovets@gmail.com>
2021-09-01 14:50:14 +0300

    config replacement

--- config.js
+++ config.js
@@ -9,35 +9,11 @@ module.exports = {
       docsPluginId: 'default',
     },
     {
-      type: 'doc',
-      docId: 'main',
-      position: 'left',
-      label: 'Members',
-      activeSidebarClassName: 'navbar__link--active',
-      docsPluginId: 'members',
-    },
-    {
-      type: 'doc',
-      docId: 'main',
-      position: 'left',
-      label: 'Seniors',
-      activeSidebarClassName: 'navbar__link--active',
-      docsPluginId: 'seniors',
-    },
-    {
-      type: 'doc',
-      docId: 'main',
-      position: 'left',
-      label: 'Executives',
-      activeSidebarClassName: 'navbar__link--active',
-      docsPluginId: 'executives',
-    },
-    {
-      href: 'http://api.touchlane.com/api/oauth/github/logout',
-      label: 'Sign Out',
-      position: 'right',
       target: '_self',
-    },
+      href: 'https://api.touchlane.com/api/oauth/github/login',
+      label: 'Sign In',
+      position: 'right',
+    }
   ],
   presets: [
     [
@@ -53,33 +29,5 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'members',
-        path: 'docs/Members',
-        routeBasePath: 'members',
-        sidebarPath: require.resolve('./sidebars.js'),
-      },
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'seniors',
-        path: 'docs/Seniors',
-        routeBasePath: 'seniors',
-        sidebarPath: require.resolve('./sidebars.js'),
-      },
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'executives',
-        path: 'docs/Executives',
-        routeBasePath: 'executives',
-        sidebarPath: require.resolve('./sidebars.js'),
-      },
-    ],
-  ],
+  plugins: [],
 }
